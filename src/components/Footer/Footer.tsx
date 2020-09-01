9c1ce159a0dea0902a3a1eead21d937f23bbfa1b
yahgwai <cpbuckland88@gmail.com>
2020-09-01 20:58:37 +0200

    Linked Uniswap in the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Text } from 'rebass'
+import { ExternalLink } from '../../theme'
 
 const Footer = styled.div<{ show: boolean }>`
   margin: 0 auto;
@@ -17,7 +18,10 @@ export default function StickyFooter() {
   return (
     <Footer show={true}>
       <Text fontSize={12} fontWeight={500} margin={'0 auto'}>
-        Forked with ♡ from Uniswap
+        Forked with ♡ from
+        <ExternalLink href="https://app.uniswap.org/">
+          <span style={{ marginLeft: '2px', color: '#FF007A' }}> Uniswap</span>
+        </ExternalLink>
       </Text>
     </Footer>
   )
