0a09282cde2a13a844af56a02193cc19cd45cb9a
Patrick McCorry <stonecoldpat@gmail.com>
2020-09-09 11:09:13 +0100

    fixed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { AbstractConnector } from '@web3-react/abstract-connector'
 import { ChainId, JSBI, Percent, Token, WETH } from '@uniswap/sdk'
 
-import { injected, walletconnect } from '../connectors'
+import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
 export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
